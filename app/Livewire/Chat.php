39006c99194f0b7d1953faa071c83fea2aae570a
Miftahurahman-STEI <mippganz@gmail.com>
2025-04-30 03:36:11 +0700

    first commit

---
+++ app/Livewire/Chat.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use App\Models\Message;
+use Livewire\Component;
+
+class Chat extends Component
+{
+    public User $user;
+
+    public $message = '';
+
+    public function render()
+    {
+        return view('livewire.chat', [
+            'messages' => Message::where('from_user_id', auth()->id())
+                        ->orWhere('from_user_id', $this->user->id)
+                        ->orWhere('to_user_id', auth()->id())
+                        ->orWhere('to_user_id', $this->user->id)
+                        ->get(),
+            
+        ]);
+    }
+
+    public function sendMessage() 
+    {
+        // dd($this->message);
+        Message::create([
+            'from_user_id' => auth()->id(),
+            'to_user_id' => $this->user->id,
+            'message' => $this->message,
+        ]);
+
+        $this->reset('message');
+    }
+}
